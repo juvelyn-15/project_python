55920ca312554361ca476a503dc4eb38b3f13902
Your Name <you@example.com>
2024-11-13 00:19:23 +0700

    -.-

--- environment.py
+++ environment.py
@@ -6,5 +6,6 @@ def install_package(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 # Install nicegui
-install_package("nicegui")
-install_package("werkzeug")
+#install_package("nicegui")
+#install_package("werkzeug")
+install_package('pandas')
